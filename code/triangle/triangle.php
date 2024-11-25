bae9c7309ca207e5a4870099b843f9e95fe010e8
CodebecqDamien <144660038+CodebecqDamien@users.noreply.github.com>
2024-11-25 16:26:55 +0000

    exercice triangle

---
+++ code/triangle/triangle.php
@@ -0,0 +1,10 @@
+<?php
+    function triangle($n){
+        for($i = 0; $i < $n; $i++){
+            for($j = 0; $j <= $i; $j++){
+                echo '*';
+            }
+            echo '<br>';
+        }
+    }
+?>

f74216a75402a4b078a4fb8105060b2f5db93f02
CodebecqDamien <144660038+CodebecqDamien@users.noreply.github.com>
2024-11-25 16:11:46 +0000

    fichier main.php terminer

--- code/Serie2/main.php
+++ code/Serie2/main.php
@@ -1,4 +1,12 @@
 <?php
-    include 'Exercice0.1.php';
+    include 'statistique.php';
 
+    $Nicolas_salaire = 2200;
+    echo "La moyenne des salaire est de : ".calcul_moyenne($note_global). "€". "<br>";
+    echo "Le salaire median est de : ".calcul_medianne($note_global). "€". "<br>";
+    if ($Nicolas_salaire > calcul_medianne($note_global)){
+        echo "Nicolas est dans les 50% des salaires les plus élevés";
+    } else {
+        echo "Nicolas est dans les 50% des salaires les moins élevés";
+    }
 ?>
--- code/Serie2/statistique.php
+++ code/Serie2/statistique.php
@@ -1,5 +1,14 @@
 <?php
-    include 'Exercice0.1.php';
+    $note_global = array(1500, 4500, 2200, 1500, 3300, 1800, 1700, 2000, 4000);
+
+    function calcul_moyenne($note_global){
+        $moyenne = 0;
+        for($i = 0; $i < count($note_global); $i++){
+            $moyenne += $note_global[$i];
+        }
+        $moyenne = $moyenne / count($note_global);
+        return $moyenne;
+    }
 
     function calcul_medianne($note_global) {
         sort($note_global);
@@ -11,5 +20,4 @@
         }
     }
 
-    echo 'La médiane est de : ' . calcul_medianne($note_global) . ' / 20.';
 ?>
